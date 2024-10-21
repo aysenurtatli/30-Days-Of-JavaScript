3b60b5551b0d1611987a647243b93b0fcfb1745a
Aysenur Tatli <aysenurtatlii@hotmail.com>
2024-10-21 17:11:12 +0300

    created answers at the exercise 1

---
+++ 01_Day_Introduction/01-solutions.js
@@ -0,0 +1,56 @@
+//1
+//comments can make code readable
+
+//2
+//Welcome to 30DaysOfJavascript
+
+//3
+/* comments can make code readable, easy to reuse and informative
+ */
+
+//4
+const string = "aysenur";
+const num = 4;
+const isTrue = true;
+const isUndefined = undefined;
+const isNull = null;
+
+//5
+console.log(typeof string);
+console.log(typeof num);
+console.log(typeof isTrue);
+console.log(typeof isUndefined);
+console.log(typeof isNull);
+
+//6
+let a;
+let b;
+let c;
+let d;
+
+//7
+const x = "x";
+const y = "y";
+const z = "z";
+const w = "w";
+
+//8
+const firstName = "Aysenur";
+const lastName = "Tatli";
+const marialStatus = false;
+const country = "Turkey";
+const age = 23;
+
+//9
+const name = "aysenur",
+  surname = "tatli",
+  isMaried = false,
+  city = "bolu",
+  year = 23;
+console.log(name, surname, isMaried, city, year);
+
+//10
+const myAge = 25;
+const yourAge = 30;
+console.log(`I am ${myAge} years old.`);
+console.log(`You are ${yourAge} years old.`);
